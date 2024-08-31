a74b031a25147f9d06e24ee1770fc1c27a9af6b4
Abubakar Shahzad <163663612+Abu-bakar56@users.noreply.github.com>
2024-08-31 12:38:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -92,8 +92,8 @@ def predict():
                            query19=request.form['query19'])
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
-
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
 
